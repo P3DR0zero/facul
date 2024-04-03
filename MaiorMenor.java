94e04f26552b23312c9328896de908376d6adcde
P3DR0zero <pedrolucas.fc.santos@gmail.com>
2024-04-03 18:51:22 -0300

    questão maior menor lista alex

---
+++ MaiorMenor.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class MaiorMenor {
+   public static int maiornum;
+    public static int menornum;
+    public static int num;
+    public static void main(String[] args) {
+       //int menornum=;
+       //int maiornum=0; 
+       int x =0;
+       int i =0;
+
+        Scanner teclado = new Scanner(System.in);
+       
+        while (x==0) {
+            for (i=0;i<5;i++){
+                System.out.print("Digite um numero:  ");
+                num=teclado.nextInt();
+
+               
+                 if (num<0) {
+                     System.out.println("Números negativos não são permitidos digite outra vez:");
+                     num=teclado.nextInt();
+                 }   
+                  else{
+                    if (i==0) {
+                        maiornum=num;
+                        menornum=num;
+                    }
+                    else{
+
+                        if (maiornum<=num) {
+                            maiornum=num;
+                           
+                         }   
+                         if (menornum>=num) {
+                             menornum=num;
+                         } 
+                    }
+
+                   System.out.println(maiornum);
+                   System.out.println(menornum);
+                
+                 }
+  
+            }
+        System.out.println("D:");
+        x= teclado.nextInt();
+        }
+
+    }
+}
+    
+    
+
+                                    
