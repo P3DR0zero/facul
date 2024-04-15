8ccdabeef06e62d7b0b7c017504593d927f7467f
P3DR0zero <pedrolucas.fc.santos@gmail.com>
2024-04-15 14:50:03 -0300

    start algoritmo(alpha1) + organização de linhas (função)

---
+++ alpha/alpha1.java
@@ -0,0 +1,13 @@
+package alpha;
+import java.util.Scanner;
+public class alpha1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);       
+    }
+
+ 
+    
+
+
+    
+}
--- alpha/função.java
+++ alpha/função.java
@@ -14,42 +14,41 @@ public class função {
         Scanner teclado = new Scanner(System.in);  
       
        switch (o) {
-        case 1 :
-            int resultado =somar( teclado.nextInt(), teclado.nextInt());
-             System.out.println(resultado);
-            break;
+             case 1 :
+                int resultado =somar( teclado.nextInt(), teclado.nextInt());
+                System.out.println(resultado);
+             break;
 
-        case 2 :
-         int result =subtração( teclado.nextInt(), teclado.nextInt());
-         System.out.println(result);
-        break;
+             case 2 :
+                int result =subtração( teclado.nextInt(), teclado.nextInt());
+                System.out.println(result);
+             break;
 
-        case 3 :
-         int resul =multiplicação( teclado.nextInt(), teclado.nextInt());
-         System.out.println(resul);        
-         break;
-
-       
-        default:
-            break;
-       }
+             case 3 :
+                int resul =multiplicação( teclado.nextInt(), teclado.nextInt());
+                System.out.println(resul);        
+             break;      
+             
+             default:
+             break;
+         }
       
     }
 
-        public static int somar (int num1, int num2){
-            int soma = num1 + num2;
-            return soma;
-        }
+            public static int somar (int num1, int num2){
+                int soma = num1 + num2;
+                return soma;
+            }
 
-        public static int subtração (int num1, int num2){
-            int subt = num1 - num2;
-            return subt;
-        }
+            public static int subtração (int num1, int num2){
+                int subt = num1 - num2;
+                return subt;
+            }
 
-        public static int multiplicação (int num1, int num2){
-            int mult = num1 * num2;
-            return mult;
-        }
+            public static int multiplicação (int num1, int num2){
+                int mult = num1 * num2;
+                return mult;
+            }
     
     
     
