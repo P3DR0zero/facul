02b733d9a0305cfeacc434c80a9291d578f2dfa0
P3DR0zero <pedrolucas.fc.santos@gmail.com>
2024-04-15 09:48:37 -0300

    teste de função

---
+++ alpha/função.java
@@ -0,0 +1,16 @@
+package alpha;
+import java.util.Scanner;
+
+
+public class função {
+    public static void main(String[] args) {
+        int resultado = somar( 3 , 5);
+        System.out.println(resultado);
+    }
+
+        public static int somar (int num1, int num2){
+            int soma = num1 + num2;
+            return soma;
+        }
+    
+}
