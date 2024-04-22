a55330ab6def121fff251915dc72ea1c62824f9d
P3DR0zero <pedrolucas.fc.santos@gmail.com>
2024-04-22 09:17:04 -0300

    import_scanner

--- alpha/alphaPet/PetShop/Animal.java
+++ alpha/alphaPet/PetShop/Animal.java
@@ -1,4 +1,5 @@
 package alpha.alphaPet.PetShop;
+import java.util.Scanner;
 
 public class Animal {
             private String dono;
@@ -19,31 +20,32 @@ public class Animal {
         System.out.println("O seu animal bebeu água.");
       }
 public static void main(String[] args) {
-   Ave k = new Ave("Karl","Pedro",30, 50, "calopsita", "vermelha");
-    k.Comer();
-    k.Beber();
-    k.Cantar();
-    k.Voar();
+    Ave k = new Ave("Karl","Pedro",30, 50, "calopsita", "vermelha");
+     k.Comer();
+     k.Beber();
+     k.Cantar();
+     k.Voar();
     
-    Cachorro e = new Cachorro("Enzo", "PL", 1, 90, "PitBull", "Albino");
-    e.Latir();
-    e.Beber();
-    e.Comer();
-    e.Correr();
-    e.Andar();
+     Cachorro e = new Cachorro("Enzo", "PL", 1, 90, "PitBull", "Albino");
+     e.Latir();
+     e.Beber();
+     e.Comer();
+     e.Correr();
+     e.Andar();
+      
 
     Dragon m=new Dragon("Manel", "Alex", 2, 1000, "Noildiana", "Azul com verde");
     m.Voar();
     m.Fogo();
 
     Gato a=new Gato("Arthur", "PL", 15, 666, "Vira Lata", "Preto pra branco não da pra saber");
-    a.Andar();
-    a.Correr();
-    a.Correr();
-    a.Pular();
+     a.Andar();
+      a.Correr();
+      a.Correr();
+      a.Pular();
 
-    Peixe y=new Peixe("Yuri", "Crack", 10, 1, "Dory", "Azul");
-    y.Nadar();
+      Peixe y=new Peixe("Yuri", "Crack", 10, 1, "Dory", "Azul");
+     y.Nadar();
 }
 
 }
--- alpha/alphaPet/PetShop/Ave.java
+++ alpha/alphaPet/PetShop/Ave.java
@@ -5,10 +5,10 @@ public class Ave extends Animal {
       private String cor_da_pena;                    
               
               public Ave(String nome, String dono, int altura, int peso,String raça, String cor_da_pena) {
-              super(nome, dono, altura, peso);
-              this.raça=raça;
-              this.cor_da_pena=cor_da_pena;
-              }
+               super(nome, dono, altura, peso);
+               this.raça=raça;
+               this.cor_da_pena=cor_da_pena;
+               }
              
               public void Voar(){
                   System.out.println("LIVRE ESTOUUU... LIVREE ESTOU, A GAIOLA NÃO VAI ME SEGURAAAAA");
